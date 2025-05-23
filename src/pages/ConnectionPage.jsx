f81a57e9ffd73d55e558763fa2d4bbc8bca8bb6c
Abhay Prasad <avirajabhay@gmail.com>
2025-05-23 20:39:15 +0530

    required pages with initial

---
+++ src/pages/ConnectionPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function ConnectionPage() {
+  return (
+    <div>ConnectionPage</div>
+  )
+}
+
+export default ConnectionPage
---
+++ src/pages/PeerDiscoveryPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function PeerDiscoveryPage() {
+  return (
+    <div>PeerDiscoveryPage</div>
+  )
+}
+
+export default PeerDiscoveryPage
